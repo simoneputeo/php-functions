bcef798206b6a2b3f7b8d7921da1cc8168a4e2a4
SimonePuteo <s.puteo@gmail.com>
2025-08-28 14:57:59 +0200

    primo commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Generatore di Password</h1>
+<form method="GET">
+  Lunghezza: <input type="number" name="length" min="8" max="64" required>
+    <br>
+  <button type="submit">Genera</button>
+  </form>
+</body>
+</html>
